ff706ccacbe33d79780af40ddca287005b71fb4b
wenxy@jugame.cn <wenxy@jugame.cn>
2018-04-20 10:41:05 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+App({
+  onLaunch: function (options) {
+    console.log('options--->',options)
+  },
+  globalData: {
+    //wxecaa3ad929e8dfd1
+    appid: 'wx1aebecae797c6598',
+    host: 'http://192.168.0.184:9020'
+    //host: 'https://weixunshi.com'
+  }
+})
