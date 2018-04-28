7cc23862a863b7f856837dac9cbe31b930ca4f03
wenxy@jugame.cn <wenxy@jugame.cn>
2018-04-28 11:05:00 +0800

    overview

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
   globalData: {
     //wxecaa3ad929e8dfd1
     appid: 'wx1aebecae797c6598',
-    host: 'http://192.168.0.184:9020'
-    //host: 'https://weixunshi.com'
+   // host: 'http://192.168.0.184:9020'
+    host: 'https://weixunshi.com'
   }
 })
